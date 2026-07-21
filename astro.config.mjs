454198e11124c534726460e2fe77be0ae05bfcd4
Nhut Nguyen <ntnhut17@gmail.com>
2026-07-21 15:58:42 +0200

    hard url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ntnhut.github.io',
-	base: '/nhut',
+	site: 'https://ntnhut.github.io/nhut/',
+	// base: '/nhut',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
